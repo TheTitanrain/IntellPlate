38e88af8b5734383769883d426fa39c8dd3ff84f
lehach <lehach@WKSADMIN>
2017-02-06 13:30:49 +1000

    1

--- IntellPlate/Form1.vb
+++ IntellPlate/Form1.vb
@@ -62,6 +62,10 @@ Public Class Form1
 
     End Sub
 
+    Private Sub BNext_Click(sender As Object, e As EventArgs) Handles BNext.Click
+
+    End Sub
+
     Private Sub DateTimePicker1_ValueChanged(sender As Object, e As EventArgs) Handles DateTimePicker1.ValueChanged
 
     End Sub
